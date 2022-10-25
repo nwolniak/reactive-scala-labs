05f5b864b6afefdaecd0e51c226844fa392ba752
nwolniak <niedzwiedz107@gmail.com>
2022-10-25 22:36:26 +0200

    lab3-solution

--- src/test/scala/EShop/lab3/TypedCartTest.scala
+++ src/test/scala/EShop/lab3/TypedCartTest.scala
@@ -1,7 +1,7 @@
 package EShop.lab3
 
-import EShop.lab2.{Cart, TypedCartActor}
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import EShop.lab2.{Cart, TypedCartActor, TypedCheckout}
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, ScalaTestWithActorTestKit, TestInbox}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpecLike
@@ -17,18 +17,32 @@ class TypedCartTest
   override def afterAll: Unit =
     testKit.shutdownTestKit()
 
-  import TypedCartActor._
-
   //use GetItems command which was added to make test easier
   it should "add item properly" in {
-    ???
+    import TypedCartActor._
+    val typedCartActor = BehaviorTestKit(new TypedCartActor().start)
+    val mainInbox = TestInbox[Cart]()
+    typedCartActor.run(AddItem("car"))
+    typedCartActor.run(GetItems(mainInbox.ref))
+    mainInbox.expectMessage(Cart.apply(Seq("car")))
   }
 
   it should "be empty after adding and removing the same item" in {
-    ???
+    import TypedCartActor._
+    val typedCartActor = testKit.spawn(new TypedCartActor().start)
+    val probe = testKit.createTestProbe[Cart]()
+    typedCartActor ! AddItem("car")
+    typedCartActor ! RemoveItem("car")
+    typedCartActor ! GetItems(probe.ref)
+    probe.expectMessage(Cart.apply(Seq.empty))
   }
 
   it should "start checkout" in {
-    ???
+    import TypedCartActor._
+    val typedCartActor = testKit.spawn(new TypedCartActor().start)
+    val probe = testKit.createTestProbe[OrderManager.Command]()
+    typedCartActor ! AddItem("car")
+    typedCartActor ! StartCheckout(probe.ref)
+    probe.expectMessageType[OrderManager.ConfirmCheckoutStarted]
   }
 }
